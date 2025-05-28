8bc7f40150cf09715f99c26f364a5065a61b5306
baselinerepo <micro.services75@gmail.com>
2025-05-28 10:13:21 +0000

    Added streaming

---
+++ examples/streaming.py
@@ -0,0 +1,67 @@
+import io
+import asyncio
+from openai import OpenAI, AsyncOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def sync_main() -> None:
+    # Initialize the OpenAI client
+    client = OpenAI()
+
+    # Model constants
+    MODEL: str = "gpt-4o-mini"
+    TEMPERATURE: float = 0.0
+
+    # Streaming:
+    # Create a sync chat completion request to the gpt-4o-mini model with streaming enabled
+    print("\n---- gpt-4o-mini Chat Completion Streaming Request ----")
+    response = client.completions.create(
+        model=MODEL,
+        max_tokens=5,
+        temperature=TEMPERATURE,
+        prompt="1,2,3,",
+        stream=True
+    )
+
+    # You can manually iterate over the response
+    first = next(response)
+    print(f"got response data: {first.to_json()}")
+
+    # Or you can automatically iterate over the response
+    for chunk in response:
+        print(chunk.to_json())
+
+
+async def async_main() -> None:
+    # Initialize the asynchronous OpenAI client
+    client = AsyncOpenAI()
+
+    # Model constants
+    MODEL: str = "gpt-4o-mini"
+    TEMPERATURE: float = 0.0
+
+    # Streaming:
+    # Create a sync chat completion request to the gpt-4o-mini model with streaming enabled
+    print("\n---- gpt-4o-mini async Chat Completion Streaming Request ----")
+    response = await client.completions.create(
+        model=MODEL,
+        max_tokens=5,
+        temperature=TEMPERATURE,
+        prompt="1,2,3,",
+        stream=True
+    )
+
+    # You can manually iterate over the response
+    # In Python 3.10+ you can also use the `await anext(response)` builtin instead
+    first = await response.__anext__()
+    print(f"got response data: {first.to_json()}")
+
+    # Or you can automatically iterate over the response
+    # Note that the for loop will not exit until *all* of the data has been processed.
+    async for chunk in response:
+        print(chunk.to_json())
+
+# run asynchronous and synchronous main functions
+sync_main()
+asyncio.run(async_main())
